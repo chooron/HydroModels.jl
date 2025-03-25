b4ea82398f6a29b66a46089aaf45dc95f701c217
jingx <50790703+chooron@users.noreply.github.com>
2025-03-25 11:46:19 +0800

    update build method and expression

--- src/utils/build.jl
+++ src/utils/build.jl
@@ -4,15 +4,14 @@ function build_flux_func(
     params::Vector{Num},
     exprs::Vector{Num},
 )
-    input_names = Symbolics.tosymbol.(inputs)
-    output_names = Symbolics.tosymbol.(outputs)
+    input_names, output_names = Symbolics.tosymbol.(inputs), Symbolics.tosymbol.(outputs)
     param_names = Symbolics.tosymbol.(params)
-    flux_exprs = toexpr.(exprs)
+    flux_exprs = toexprv2.(unwrap.(exprs))
     input_assign_calls = [:($i = inputs[$idx]) for (idx, i) in enumerate(input_names)]
-    params_assign_calls = [:($p = params[$idx]) for (idx, p) in enumerate(param_names)]
-    compute_calls = [:($target = @. $expr) for (target, expr) in zip(output_names, flux_exprs)]
+    params_assign_calls = [:($p = pas.params.$p) for p in param_names]
+    compute_calls = [:($o = $expr) for (o, expr) in zip(output_names, flux_exprs)]
     return_calls = :(return [$(output_names...)])
-    flux_func_expr = :(function (inputs, params)
+    flux_func_expr = :(function (inputs, pas)
         $(input_assign_calls...)
         $(params_assign_calls...)
         $(compute_calls...)
@@ -22,7 +21,30 @@ function build_flux_func(
     return generated_flux_func
 end
 
-function build_ele_func(
+# function build_multi_flux_func(
+#     inputs::Vector{Num},
+#     outputs::Vector{Num},
+#     params::Vector{Num},
+#     exprs::Vector{Num},
+# )
+#     input_names, output_names = Symbolics.tosymbol.(inputs), Symbolics.tosymbol.(outputs)
+#     param_names = Symbolics.tosymbol.(params)
+#     flux_exprs = toexprv2.(unwrap.(exprs))
+#     input_assign_calls = [:($i = inputs[$idx, :, :]) for (idx, i) in enumerate(input_names)]
+#     params_assign_calls = [:($p = pas.params.$p) for p in param_names]
+#     compute_calls = [:($o = $expr) for (o, expr) in zip(output_names, flux_exprs)]
+#     return_calls = :(return [$(output_names...)])
+#     flux_func_expr = :(function (inputs, pas)
+#         $(input_assign_calls...)
+#         $(params_assign_calls...)
+#         $(compute_calls...)
+#         $(return_calls)
+#     end)
+#     generated_flux_func = @RuntimeGeneratedFunction(flux_func_expr)
+#     return generated_flux_func
+# end
+
+function build_single_ele_func(
     fluxes::Vector{<:AbstractFlux},
     dfluxes::Vector{<:AbstractStateFlux},
     meta::ComponentVector,
@@ -30,17 +52,9 @@ function build_ele_func(
     input_names, output_names = tosymbol.(meta.inputs), tosymbol.(meta.outputs)
     state_names, param_names = tosymbol.(meta.states), tosymbol.(meta.params)
     nfluxes = filter(f -> f isa AbstractNeuralFlux, fluxes)
-    funcs_nns_bounds = []
-    #* prepare nn bounds
-    if !isempty(nfluxes)
-        funcs_nns_len = [length(nflux.nninfos[:params]) for nflux in nflux]
-        start_indices = [1; cumsum(funcs_nns_len)[1:end-1] .+ 1]
-        end_indices = cumsum(funcs_nns_len)
-        funcs_nns_bounds = [start:stop for (start, stop) in zip(start_indices, end_indices)]
-    end
 
-    params_assign_calls = [:($p = params[$idx]) for (idx, p) in enumerate(param_names)]
-    nn_params_assign_calls = [:($p = nn_params[$idx]) for (idx, p) in zip(funcs_nns_bounds, [nflux.nninfos[:params] for nflux in nfluxes])]
+    params_assign_calls = [:($p = pas.params.$p) for p in param_names]
+    nn_params_assign_calls = [:($nn = pas.nns.$nn) for nn in [nflux.nninfos[:params] for nflux in nfluxes]]
 
     # varibles definitions expressions
     diff_define_calls = reduce(vcat, [
@@ -57,8 +71,7 @@ function build_ele_func(
         params_assign_calls, nn_params_assign_calls,
     ])
 
-    state_compute_calls = []
-    flux_compute_calls = []
+    state_compute_calls, flux_compute_calls = [], []
     for f in fluxes
         if f isa AbstractNeuralFlux
             push!(state_compute_calls, :($(f.nninfos[:outputs]) = f.func($(f.nninfos[:inputs]), $(f.nninfos[:params]))))
@@ -67,25 +80,25 @@ function build_ele_func(
             append!(flux_compute_calls, [:($(nm) = $(f.nninfos[:outputs])[i]) for (i, nm) in enumerate(get_output_names(f))])
         else
             append!(state_compute_calls, [:($nm = $(toexpr(expr))) for (nm, expr) in zip(get_output_names(f), f.exprs)])
-            append!(flux_compute_calls, [:($nm = $(toexprv2(unwrap(expr), LazyState()))) for (nm, expr) in zip(get_output_names(f), f.exprs)])
+            append!(flux_compute_calls, [:($nm = $(toexprv2(unwrap(expr)))) for (nm, expr) in zip(get_output_names(f), f.exprs)])
         end
     end
 
     # Create return expressions with concrete values
     return_flux = :(return [$(output_names...)])
-    return_state = :(return [$(map(expr -> :($(toexpr(expr))), reduce(vcat, [dflux.exprs for dflux in dfluxes]))...)])
+    return_state = :(return [$(map(expr -> :($(toexpr(expr))), reduce(vcat, get_exprs.(dfluxes)))...)])
 
     # Add meta information for optimization
     meta_exprs = [:(Base.@_inline_meta)]
 
-    flux_func_expr = :(function (inputs, states, params, nn_params)
+    flux_func_expr = :(function (inputs, states, pas)
         $(meta_exprs...)
         $(flux_define_calls...)
         $(flux_compute_calls...)
         $(return_flux)
     end)
 
-    diff_func_expr = :(function (inputs, states, params, nn_params)
+    diff_func_expr = :(function (inputs, states, pas)
         $(meta_exprs...)
         $(diff_define_calls...)
         $(state_compute_calls...)
@@ -98,76 +111,70 @@ function build_ele_func(
 end
 
 
-#* 构建bucket的函数
-function build_ele_func_old(
-    funcs::Vector{<:AbstractFlux},
-    dfuncs::Vector{<:AbstractStateFlux},
+function build_multi_ele_func(
+    fluxes::Vector{<:AbstractFlux},
+    dfluxes::Vector{<:AbstractStateFlux},
     meta::ComponentVector,
 )
-    #* prepare variables for function building
-    funcs_inputs, funcs_states, funcs_params = meta.inputs, meta.states, meta.params
-    #* prepare Assignment and Output Variables
-    assign_list, output_list, funcs_nns = Assignment[], Num[], []
-    for func in funcs
-        if func isa AbstractNeuralFlux
-            push!(funcs_nns, func.nninfos[:nns])
-            #* For NeuralFlux, use nn_input to match the input variable
-            push!(assign_list, Assignment(func.nninfos[:inputs], MakeArray(get_input_vars(func), Vector)))
-            #* For NeuralFlux, use nn_output to match the calculation result of nn expr
-            push!(assign_list, Assignment(func.nninfos[:outputs], get_exprs(func)[1]))
-            #* According to the output variable name, match each index of nn_output
-            for (idx, output) in enumerate(get_output_vars(func))
-                push!(assign_list, Assignment(output, func.nninfos[:outputs][idx]))
-                push!(output_list, output)
-            end
+    input_names, output_names = tosymbol.(meta.inputs), tosymbol.(meta.outputs)
+    state_names, param_names = tosymbol.(meta.states), tosymbol.(meta.params)
+    nfluxes = filter(f -> f isa AbstractNeuralFlux, fluxes)
+
+    params_assign_calls = [:($p = pas.params.$p) for p in param_names]
+    nn_params_assign_calls = [:($nn = pas.nns.$nn) for nn in [nflux.nninfos[:params] for nflux in nfluxes]]
+
+    # varibles definitions expressions
+    diff_define_calls = reduce(vcat, [
+        [:($i = inputs[$idx, :]) for (idx, i) in enumerate(input_names)],
+        [:($s = states[$idx, :]) for (idx, s) in enumerate(state_names)],
+        [:($(f.nninfos[:inputs]) = permutedims(reduce(hcat, [$(get_input_names(f)...)]))) for f in nfluxes],
+        params_assign_calls, nn_params_assign_calls,
+    ])
+
+    flux_define_calls = reduce(vcat, [
+        [:($i = inputs[$idx, :, :]) for (idx, i) in enumerate(input_names)],
+        [:($s = states[$idx, :, :]) for (idx, s) in enumerate(state_names)],
+        [:($(f.nninfos[:inputs]) = permutedims(reduce(hcat, [$(get_input_names(f)...)]))) for f in nfluxes],
+        params_assign_calls, nn_params_assign_calls,
+    ])
+
+    state_compute_calls, flux_compute_calls = [], []
+    for f in fluxes
+        if f isa AbstractNeuralFlux
+            push!(state_compute_calls, :($(f.nninfos[:outputs]) = f.func($(f.nninfos[:inputs]), $(f.nninfos[:params]))))
+            append!(state_compute_calls, [:($(nm) = $(f.nninfos[:outputs])[i]) for (i, nm) in enumerate(get_output_names(f))])
+            push!(flux_compute_calls, :($(f.nninfos[:outputs]) = f.func($(f.nninfos[:inputs]), $(f.nninfos[:params]))))
+            append!(flux_compute_calls, [:($(nm) = $(f.nninfos[:outputs])[i]) for (i, nm) in enumerate(get_output_names(f))])
         else
-            #* According to the output variable name, match each result of the flux exprs
-            for (output, expr) in zip(get_output_vars(func), get_exprs(func))
-                push!(assign_list, Assignment(output, expr))
-                push!(output_list, output)
-            end
+            append!(state_compute_calls, [:($nm = $(toexprv2(unwrap(expr)))) for (nm, expr) in zip(get_output_names(f), f.exprs)])
+            append!(flux_compute_calls, [:($nm = $(toexprv2(unwrap(expr)))) for (nm, expr) in zip(get_output_names(f), f.exprs)])
         end
     end
 
-    funcs_nns_bounds = []
-    #* prepare nn bounds
-    if !isempty(funcs_nns)
-        funcs_nns_len = [length(funcs_nns[k]) for k in keys(funcs_nns)]
-        start_indices = [1; cumsum(funcs_nns_len)[1:end-1] .+ 1]
-        end_indices = cumsum(funcs_nns_len)
-        funcs_nns_bounds = [start:stop for (start, stop) in zip(start_indices, end_indices)]
-    end
+    # Create return expressions with concrete values
+    return_flux = :(return [$(output_names...)])
+    return_state = :(return [$(map(expr -> :($(toexprv2(unwrap(expr)))), reduce(vcat, get_exprs.(dfluxes)))...)])
 
-    #* convert flux output to array (include states and outputs)
-    flux_output_array = MakeArray(vcat(funcs_states, output_list), Vector)
+    # Add meta information for optimization
+    meta_exprs = [:(Base.@_inline_meta)]
 
-    #* Set the input argument of ODE Function
-    func_args = [
-        #* argument 1: Function input variables
-        DestructuredArgs(funcs_inputs, :inputs, inbounds=true),
-        #* argument 2: Function state variables
-        DestructuredArgs(funcs_states, :states, inbounds=true),
-        #* argument 2: Function calculation parameters
-        DestructuredArgs(funcs_params, :params, inbounds=true),
-        #* argument 3: Function neuralnetwork parameters
-        DestructuredArgs(funcs_nns, :nns, inds=funcs_nns_bounds, inbounds=true),
-    ]
+    flux_func_expr = :(function (inputs, states, pas)
+        $(meta_exprs...)
+        $(flux_define_calls...)
+        $(flux_compute_calls...)
+        $(return_flux)
+    end)
 
-    #* Construct Flux Function: Func(args, kwargs, body), where body represents the matching formula between each variable and expression
-    generated_flux_func = @RuntimeGeneratedFunction(
-        toexpr(Func(func_args, [], Let(assign_list, flux_output_array, false)))
-    )
+    diff_func_expr = :(function (inputs, states, pas)
+        $(meta_exprs...)
+        $(diff_define_calls...)
+        $(state_compute_calls...)
+        $(return_state)
+    end)
 
-    if !isempty(dfuncs)
-        #* convert diff state output to array
-        diffst_output_array = MakeArray(reduce(vcat, get_exprs.(dfuncs)), Vector)
-        generated_diff_func = @RuntimeGeneratedFunction(
-            toexpr(Func(func_args, [], Let(assign_list, diffst_output_array, false)))
-        )
-    else
-        generated_diff_func = nothing
-    end
-    generated_flux_func, generated_diff_func
+    generated_flux_func = @RuntimeGeneratedFunction(flux_func_expr)
+    generated_diff_func = @RuntimeGeneratedFunction(diff_func_expr)
+    return generated_flux_func, generated_diff_func
 end
 
 function build_route_func(
--- src/utils/expression.jl
+++ src/utils/expression.jl
@@ -1,70 +1,33 @@
 # git from https://github.com/JuliaSymbolics/SymbolicUtils.jl/blob/master/src/code.jl
-struct NameState
-    rewrites::Dict{Any,Any}
-end
-NameState() = NameState(Dict{Any,Any}())
-function union_rewrites!(n, ts)
-    for t in ts
-        n[t] = Symbol(string(t))
-    end
-end
+toexprv2(x) = toexprv2(x, SymbolicUtils.Code.LazyState())
 
-struct LazyState
-    ref::Ref{Any}
-end
-LazyState() = LazyState(Ref{Any}(nothing))
-
-function Base.get(st::LazyState)
-    s = getfield(st, :ref)[]
-    s === nothing ? getfield(st, :ref)[] = NameState() : s
-end
-
-@inline Base.getproperty(st::LazyState, f::Symbol) = f == :symbolify ? getproperty(st, :rewrites) : getproperty(get(st), f)
-
-toexprv2(x) = toexprv2(x, LazyState())
-
-const NaNMathFuns = (
-    sin,
-    cos,
-    tan,
-    asin,
-    acos,
-    acosh,
-    atanh,
-    log,
-    log2,
-    log10,
-    lgamma,
-    log1p,
-    sqrt,
-)
-function function_to_expr(op, O, st)
-    (get(st.rewrites, :nanmath, false) && op in NaNMathFuns) || return nothing
+function function_to_exprv2(op, O, st)
+    (get(st.rewrites, :nanmath, false) && op in SymbolicUtils.Code.NaNMathFuns) || return nothing
     name = nameof(op)
-    fun = GlobalRef(NaNMath, name)
+    fun = GlobalRef(SymbolicUtils.Code.NaNMath, name)
     args = map(Base.Fix2(toexprv2, st), arguments(O))
     expr = Expr(:call, fun)
     append!(expr.args, args)
     return expr
 end
 
-function function_to_expr(op::Union{typeof(*),typeof(+)}, O, st)
+function function_to_exprv2(op::Union{typeof(*),typeof(+)}, O, st)
     out = get(st.rewrites, O, nothing)
     out === nothing || return out
     args = map(Base.Fix2(toexprv2, st), sorted_arguments(O))
     if length(args) >= 3 && symtype(O) <: Number
         x, xs = Iterators.peel(args)
         foldl(xs, init=x) do a, b
-            Expr(:call, :broadcast, op, a, b)
+            Expr(:call, :broadcast, op, a, b) # add broadcast
         end
     else
-        expr = Expr(:call, :broadcast, op)
+        expr = Expr(:call, :broadcast, op) # add broadcast
         append!(expr.args, args)
         expr
     end
 end
 
-function function_to_expr(op::typeof(^), O, st)
+function function_to_exprv2(op::typeof(^), O, st)
     args = arguments(O)
     if args[2] isa Real && args[2] < 0
         args[1] = Term(inv, Any[args[1]])
@@ -74,18 +37,18 @@ function function_to_expr(op::typeof(^), O, st)
         return toexprv2(args[1], st)
     end
     if get(st.rewrites, :nanmath, false) === true && !(args[2] isa Integer)
-        op = NaNMath.pow
+        op = SymbolicUtils.Code.NaNMath.pow
         return toexprv2(Term(op, args), st)
     end
     return nothing
 end
 
-function function_to_expr(::typeof(ifelse), O, st)
+function function_to_exprv2(::typeof(ifelse), O, st)
     args = arguments(O)
     :($(toexprv2(args[1], st)) ? $(toexprv2(args[2], st)) : $(toexprv2(args[3], st)))
 end
 
-function function_to_expr(x::BasicSymbolic, O, st)
+function function_to_exprv2(x::BasicSymbolic, O, st)
     issym(x) ? get(st.rewrites, O, nothing) : nothing
 end
 
@@ -108,7 +71,7 @@ function toexprv2(O, st)
 
     !iscall(O) && return O
     op = operation(O)
-    expr′ = function_to_expr(op, O, st)
+    expr′ = function_to_exprv2(op, O, st)
     if expr′ !== nothing
         return expr′
     else
