117ac5b64eac44cc9768a237d6dcead300d1dc97
jingx <50790703+chooron@users.noreply.github.com>
2025-04-09 14:04:04 +0800

    move to HydroModelCore

--- lib/HydroModelCore/src/HydroModelCore.jl
+++ lib/HydroModelCore/src/HydroModelCore.jl
@@ -1,3 +1,25 @@
+module HydroModelCore
+
+using ModelingToolkit: Num
+using Symbolics: tosymbol
+## Abstract Component Types
+abstract type AbstractComponent end
+
+abstract type AbstractFlux <: AbstractComponent end
+abstract type AbstractHydroFlux <: AbstractFlux end
+abstract type AbstractNeuralFlux <: AbstractHydroFlux end
+abstract type AbstractStateFlux <: AbstractFlux end
+
+abstract type AbstractElement <: AbstractComponent end
+abstract type AbstractBucket <: AbstractElement end
+abstract type AbstractHydrograph <: AbstractElement end
+abstract type AbstractRoute <: AbstractElement end
+abstract type AbstractHydroRoute <: AbstractRoute end
+abstract type AbstractModel <: AbstractComponent end
+
+abstract type AbstractNNLayer <: AbstractComponent end
+abstract type AbstractNNModel <: AbstractComponent end
+
 """
     get(cpt::AbstractComponent)::Num
 
@@ -100,3 +122,5 @@ function get_vars(components::Vector{<:AbstractComponent})
     end
     inputs, outputs, states
 end
+
+end # module HydroModelCore
