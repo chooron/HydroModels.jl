cb26c40ebae3644a75424ac8ab48b5bb3225130d
jingx <50790703+chooron@users.noreply.github.com>
2025-04-10 11:26:36 +0800

    Update HydroModelWrappers.jl

--- lib/HydroModelWrappers/src/HydroModelWrappers.jl
+++ lib/HydroModelWrappers/src/HydroModelWrappers.jl
@@ -2,4 +2,17 @@ module HydroModelWrappers
 
 using Chain
 
+abstract type AbstractHydroWrapper end
+abstract type AbstractDataPreprocessor <: AbstractHydroWrapper end
+abstract type AbstractDataPostprocessor <: AbstractHydroWrapper end
+abstract type AbstractComponentDecorator <: AbstractHydroWrapper end
+
+include("namedtuple_wrapper.jl")
+# include("estimate_params.jl")
+# include("neural_wrapper.jl")
+# include("record_states.jl")
+include("stats_outlet.jl")
+
+export NamedTuplePreprocessor, NamedTuplePostprocessor, SelectComponentOutlet
+
 end # module HydroModelWrappers
