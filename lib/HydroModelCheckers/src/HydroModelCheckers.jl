84927b7f321176dad3b76986bd0a6b7a1a7153c4
jingx <50790703+chooron@users.noreply.github.com>
2025-04-10 20:07:19 +0800

    move libs into HydroModelTools

--- lib/HydroModelCheckers/src/HydroModelCheckers.jl
+++
@@ -1,9 +0,0 @@
-module HydroModelCheckers
-
-using ComponentArrays
-using HydroModelCore: AbstractComponent
-
-include("check_params.jl")
-include("check_sort.jl")
-
-end # module HydroModelCheckers
