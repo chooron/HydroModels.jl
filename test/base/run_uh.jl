59a948b17a1d2524ad380860bd219c88998bd8f1
jingx <50790703+chooron@users.noreply.github.com>
2025-04-13 21:46:17 +0800

    Update run_uh.jl

--- test/base/run_uh.jl
+++ test/base/run_uh.jl
@@ -1,22 +1,32 @@
 @testset "test unit hydro flux" begin
     # Define the variables and parameters
-    @variables q1 q1_routed
+    @variables q1 q1_lag
     @parameters x1
-    # Define a unit hydrograph function
-    uh_func = HydroModels.UHFunction(:UH_1_HALF)
     # Create a UnitHydroRouteFlux object
     # Input: q1 (flow)
     # Parameter: x1 (routing parameter)
     # Using uh_1_half as the unit hydrograph function
     # Solve type: unithydro1 (convolution method)
-    uh1 = HydroModels.UnitHydrograph([q1] => [q1_routed], [x1], uhfunc=uh_func, solvetype=:DISCRETE)
-    uh2 = HydroModels.UnitHydrograph(q1, q1_routed, [x1], uhfunc=uh_func, solvetype=:SPARSE)
+    uh1 =  HydroModels.@unithydro :maxbas_uh begin
+        uh_func = begin
+            lag => (t / lag)^2.5
+        end
+        uh_vars = [q1]
+        configs = (solvetype=:SPARSE, suffix=:_lag)
+    end
+    uh1 =  HydroModels.@unithydro :maxbas_uh begin
+        uh_func = begin
+            lag => (t / lag)^2.5
+        end
+        uh_vars = [q1]
+        configs = (solvetype=:SPARSE, suffix=:_lag)
+    end
     # Test the input names of the router
     @test HydroModels.get_input_names(uh1) == HydroModels.get_input_names(uh2) == [:q1]
     # Test the parameter names of the router
-    @test HydroModels.get_param_names(uh1) == HydroModels.get_param_names(uh2) == [:x1]
+    @test HydroModels.get_param_names(uh1) == HydroModels.get_param_names(uh2) == [:lag]
     # Test the output names of the router
-    @test HydroModels.get_output_names(uh1) == HydroModels.get_output_names(uh2) == [:q1_routed]
+    @test HydroModels.get_output_names(uh1) == HydroModels.get_output_names(uh2) == [:q1_lag]
     # Test the routing function with sample input
     input_flow = Float32[2 3 4 2 3 1]
     params = ComponentVector(params=(x1=3.5,))
@@ -27,7 +37,7 @@
     # test with multiple nodes
     input_arr = repeat(reshape(input_flow, 1, 1, length(input_flow)), 1, 10, 1)
     ndtypes = [Symbol("node_$i") for i in 1:10]
-    input_pas = ComponentVector(params=(x1=fill(3.5, 10),),)
+    input_pas = ComponentVector(params=(lag=fill(3.5, 10),),)
     config = Dict(:ptypes=>ndtypes, :solver=>ManualSolver(mutable=true))
     expected_output_arr = repeat(reshape(expected_output, 1, 1, length(expected_output)), 1, 10, 1)
     @test uh1(input_arr, input_pas; config...) ≈ expected_output_arr atol = 1e-3
