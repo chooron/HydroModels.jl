795e3114de9e3abd0e038c86f39ff3061889e790
jing xin <50790703+chooron@users.noreply.github.com>
2025-11-09 16:52:11 +0800

    Remove outdated neural docs and update dependencies
    Deleted obsolete documentation files related to neural bucket and components. Updated Manifest.toml, Project.toml, and make.jl to reflect new Julia and package versions for improved compatibility and maintenance.

--- docs/make.jl
+++ docs/make.jl
@@ -48,49 +48,6 @@ makedocs(
     ]
 )
 
-# # Chinese Documentation
-# makedocs(
-#     sitename = "HydroModels.jl",
-#     authors = "xin jing",
-#     format = Documenter.HTML(
-#         # 启用 pretty URLs，移除 .html 后缀
-#         # 设置文档的规范 URL
-#         canonical = "https://chooron.github.io/HydroModels.jl/dev",
-#         # 配置侧边栏
-#         collapselevel = 2,
-#         sidebar_sitename = true
-#     ),
-#     # 配置模块
-#     modules = [HydroModels],
-#     clean = true,
-#     doctest = false,
-#     linkcheck = true,
-#     source = "src",
-#     build = "build_zh",
-#     lang = "zh",
-#     # 配置页面结构
-#     pages = [
-#         "主页" => "index.md",
-#         "入门指南" => "getting_started.md",
-#         "基本概念" => [
-#             "蓄水模型" => "concepts/bucket_model.md",
-#             "通量模型" => "concepts/flux_model.md",
-#             "汇流模型" => "concepts/route_model.md",
-#             "求解器模型" => "concepts/solver_model.md",
-#             "分布式模型" => "concepts/spatial_model.md",
-#             "集总式模型" => "concepts/lumped_model.md",
-#         ],
-#         "模型构建" => [
-#             "构建 ExpHydro 模型" => "implements/build_exphydro_model_zh.md",
-#             "构建 M50 模型" => "implements/build_m50_model_zh.md",
-#             "构建河道汇流模型" => "implements/build_discharge_route_zh.md",
-#         ],
-#         "模型运行" => [
-#             "运行蓄水模型" => "tutorials/run_a_bucket.md",
-#             "运行 ExpHydro 模型" => "tutorials/run_a_exphydro_model.md"
-#         ]
-#     ]
-# )
 
 # 部署配置
 deploydocs(
@@ -99,10 +56,3 @@ deploydocs(
     push_preview = true,
     target = "build_en"  # 确保这里指定了正确的构建目录
 )
-
-# deploydocs(
-#     repo = "github.com/chooron/HydroModels.jl",
-#     devbranch = "main",
-#     push_preview = true,
-#     target = "build_zh"  # 确保这里指定了正确的构建目录
-# )
